721a15e37c46885ebed464138f489a9a61530e6f
ishi <panchomorales7581@gmail.com>
2025-08-31 11:51:53 -0600

    first commit

---
+++ Tarea1/Controllers/Ejercicio1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.Encodings.Web;
+
+namespace Tarea1.Controllers
+{
+	public class Ejercicio1 : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public string Convertir(string cadena)
+		{
+			// Convertir a mayusculas e invertir la cadena
+			string cadenaMayusculasInvertida = "";
+			cadena.ToUpper().Reverse().ToList().ForEach(c => cadenaMayusculasInvertida += c);
+
+			return HtmlEncoder.Default.Encode($"Cadena al reves y en mayusculas: {cadenaMayusculasInvertida}");
+		}
+
+		public string Comparar(string cadena1, string cadena2)
+		{
+			bool result = cadena1.Equals(cadena2);
+			if (result == false)
+				return HtmlEncoder.Default.Encode($"Las cadenas no son iguales");
+			else
+				return HtmlEncoder.Default.Encode($"Las cadenas son iguales");
+		}
+
+		public IActionResult Perfil()
+		{
+			return View();
+		}
+	}
+}
---
+++ Tarea1/Controllers/Ejercicio2.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tarea1.Controllers
+{
+	public class Ejercicio2 : Controller
+	{
+		private List<string> nombres = new List<string>
+		{
+            "Valeria", "Andrés", "Camila", "Sebastian", "Mariana", "Gabriel", "Daniela", "Fernando", "Natalia", "Pancho"
+		};
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public string BuscarPosicion(int posicion)
+		{
+			if (posicion < 0 || posicion >= nombres.Count)
+			{
+				return "Posición inválida";
+			}
+
+			return $"El nombre de la posicion '{posicion}' es: '{nombres[posicion]}' ";
+            
+        }
+
+		public int BuscarCadena(string nombre)
+		{
+            return nombres.FindIndex(n => n.Equals(nombre));
+        }
+	}
+}
